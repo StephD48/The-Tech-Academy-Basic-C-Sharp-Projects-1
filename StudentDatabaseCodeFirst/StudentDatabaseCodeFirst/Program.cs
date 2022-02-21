9c633916f9f86d11c950292e20636613df6b89e8
Stephanie Drakegit config --global user.email stephlynnd37@gmail.com <93282155+StephD48@users.noreply.github.com>
2022-02-21 12:23:37 -0800

    Added new files

---
+++ StudentDatabaseCodeFirst/StudentDatabaseCodeFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+
+namespace StudentDatabaseCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Sally " };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
